4212b0e9f7de789357e2d5c648684a120dd26196
Slayer Bryan <bryan.antonio.alvarado.villalobos@gmail.com>
2019-12-15 23:02:29 -0500

    Commit Inicial

---
+++ src/test/java/com/slayer/api/main/SpringClientesAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slayer.api.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringClientesAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
